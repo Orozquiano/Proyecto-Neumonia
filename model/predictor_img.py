c7a512e7e752106f2db6351af37ee902767e23d3
Manuel <manuel9391@gmail.com>
2026-02-12 01:18:18 -0500

    Aplicando cohesion

--- model/predictor_img.py
+++ model/predictor_img.py
@@ -12,10 +12,13 @@ class Predictor:
         2: "viral"
     }
 
-    def __init__(self, model):
+    def __init__(self, model, use_gradcam=True):
         self.model = model
         self.preprocessor = Preprocessor()
-        self.gradcam = GradCAM(model)
+        self.use_gradcam = use_gradcam
+
+        if use_gradcam:
+            self.gradcam = GradCAM(model)
 
     def predict(self, array):
         
@@ -29,8 +32,11 @@ class Predictor:
         label = ["bacteriana", "normal", "viral"][prediction]
 
         # Grad-CAM usando la imagen procesada
-        heatmap = self.gradcam.grad_cam(processed_img, original_image=array)
-
+        # heatmap = self.gradcam.grad_cam(processed_img, original_image=array)
+        if self.use_gradcam:
+            heatmap = self.gradcam.grad_cam(processed_img, original_image=array)
+        else:
+            heatmap = None
         return label, proba, heatmap
 
     
