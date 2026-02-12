d9d07d8f927a9cab5813f2627e92c816307c3da2
Manuel <manuel9391@gmail.com>
2026-02-12 01:19:17 -0500

    Aplicando cohesion

--- tests/test_predictor.py
+++ tests/test_predictor.py
@@ -1,13 +1,34 @@
-from image.preprocess_img import Preprocessor
-from model.grad_cam import GradCAM
+import sys
+import os
+# Asegurarse de que la raíz del proyecto esté en el path para que 'src' sea reconocible
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import numpy as np
+try:
+    from src.model.predictor_img import Predictor
+except ImportError:
+    # Intento alternativo si se ejecuta desde dentro de la carpeta tests o si la estructura varía
+    from model.predictor_img import Predictor
 
 
-class Predictor:
-    
-    def __init__(self, model, use_gradcam=True):
-        self.model = model
-        self.preprocessor = Preprocessor()
-        self.use_gradcam = use_gradcam
+class DummyLayer:
+    def __init__(self, name):
+        self.name = name
 
-        if use_gradcam:
-            self.gradcam = GradCAM(model)
+class DummyModel:
+    def __init__(self):
+        self.layers = [DummyLayer("conv_1"), DummyLayer("dense_1")]
+
+    def __call__(self, inputs, training=False):
+        return np.array([[0.1, 0.8, 0.1]])
+
+def test_predict_returns_valid_output():
+
+    predictor = Predictor(model=DummyModel(), use_gradcam=False)
+
+    fake_image = np.random.randint(0, 255, (1024, 1024, 3), dtype=np.uint8)
+
+    label, proba, heatmap = predictor.predict(fake_image)
+
+    assert label in ["bacteriana", "normal", "viral"]
+    assert 0 <= proba <= 100
